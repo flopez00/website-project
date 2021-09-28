322bc3d90b3d51e0a86d7368f4452a8d3652262b
Francisco <franciscolopezjr012@gmail.com>
2021-09-28 00:58:41 -0500

    more coded added

---
+++ script.js
@@ -0,0 +1,12 @@
+const myButton = document.querySelector('.signUpBtn');
+const myButton2 = document.querySelector('.signUpBtn2');
+
+
+myButton.onclick = function signedUp () {
+    alert('You have signed up successfully!');
+    myButton2.onclick = function signedUp () {
+        alert('You have signed up successfully!');
+    
+    }
+}
+
